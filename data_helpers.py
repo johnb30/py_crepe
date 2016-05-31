cdb94b2c1cb3d570b935d2442981fa1824fbde96
John Beieler <john.b30@gmail.com>
2016-05-31 11:20:43 -0400

    Initial commit.

---
+++ data_helpers.py
@@ -0,0 +1,90 @@
+import string
+import numpy as np
+import pandas as pd
+from keras.utils.np_utils import to_categorical
+
+
+def load_ag_data():
+    train = pd.read_csv('data/ag_news_csv/train.csv', header=None)
+    train = train.dropna()
+
+    x_train = train[1] + train[2]
+    x_train = np.array(x_train)
+
+    y_train = train[0] - 1
+    y_train = to_categorical(y_train)
+
+    test = pd.read_csv('data/ag_news_csv/test.csv', header=None)
+    x_test = test[1] + test[2]
+    x_test = np.array(x_test)
+
+    y_test = test[0] - 1
+    y_test = to_categorical(y_test)
+
+    return (x_train, y_train), (x_test, y_test)
+
+
+def mini_batch_generator(x, y, vocab, vocab_size, vocab_check, maxlen,
+                         batch_size=128):
+
+    for i in xrange(0, len(x), batch_size):
+        x_sample = x[i:i + batch_size]
+        y_sample = y[i:i + batch_size]
+
+        input_data = encode_data(x_sample, maxlen, vocab, vocab_size,
+                                 vocab_check)
+
+        yield (input_data, y_sample)
+
+
+def encode_data(x, maxlen, vocab, vocab_size, check):
+    #Iterate over the loaded data and create a matrix of size maxlen x vocabsize
+    #In this case that will be 1014x69. This is then placed in a 3D matrix of size
+    #data_samples x maxlen x vocab_size. Each character is encoded into a one-hot
+    #array. Chars not in the vocab are encoded into an all zero vector.
+
+    input_data = np.zeros((len(x), maxlen, vocab_size))
+    for dix, sent in enumerate(x):
+        counter = 0
+        sent_array = np.zeros((maxlen, vocab_size))
+        chars = list(sent.lower().replace(' ', ''))
+        for c in chars:
+            if counter >= 1014:
+                pass
+            else:
+                char_array = np.zeros(vocab_size, dtype=np.int)
+                if c in check:
+                    ix = vocab[c]
+                    char_array[ix] = 1
+                sent_array[counter, :] = char_array
+                counter += 1
+        input_data[dix, :, :] = sent_array
+
+    return input_data
+
+
+def shuffle_matrix(x, y):
+    stacked = np.hstack((np.matrix(x).T, y))
+    np.random.shuffle(stacked)
+    xi = np.array(stacked[:, 0]).flatten()
+    yi = np.array(stacked[:, 1:])
+
+    return xi, yi
+
+
+def create_vocab_set():
+    #This alphabet is 69 chars vs. 70 reported in the paper since they include two
+    # '-' characters. See https://github.com/zhangxiangxiao/Crepe#issues.
+
+    alphabet = (list(string.ascii_lowercase) + list(string.digits) +
+                list(string.punctuation) + ['\n'])
+    vocab_size = len(alphabet)
+    check = set(alphabet)
+
+    vocab = {}
+    reverse_vocab = {}
+    for ix, t in enumerate(alphabet):
+        vocab[t] = ix
+        reverse_vocab[ix] = t
+
+    return vocab, reverse_vocab, vocab_size, check
---
+++ py_crepe.py
@@ -0,0 +1,53 @@
+'''
+Run on GPU: THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python py_crepe.py
+'''
+
+from keras.models import Model
+from keras.optimizers import SGD
+from keras.layers import Input, Dense, Dropout, Flatten
+from keras.layers.convolutional import Convolution1D, MaxPooling1D
+
+
+def model(filter_kernels, dense_outputs, maxlen, vocab_size, nb_filter,
+          cat_output):
+    #Define what the input shape looks like
+    inputs = Input(shape=(maxlen, vocab_size), name='input', dtype='float32')
+
+    #All the convolutional layers...
+    conv = Convolution1D(nb_filter=nb_filter, filter_length=filter_kernels[0],
+                         border_mode='valid', activation='relu',
+                         input_shape=(maxlen, vocab_size))(inputs)
+    conv = MaxPooling1D(pool_length=3)(conv)
+
+    conv1 = Convolution1D(nb_filter=nb_filter, filter_length=filter_kernels[1],
+                          border_mode='valid', activation='relu')(conv)
+    conv1 = MaxPooling1D(pool_length=3)(conv1)
+
+    conv2 = Convolution1D(nb_filter=nb_filter, filter_length=filter_kernels[2],
+                          border_mode='valid', activation='relu')(conv1)
+
+    conv3 = Convolution1D(nb_filter=nb_filter, filter_length=filter_kernels[3],
+                          border_mode='valid', activation='relu')(conv2)
+
+    conv4 = Convolution1D(nb_filter=nb_filter, filter_length=filter_kernels[4],
+                          border_mode='valid', activation='relu')(conv3)
+
+    conv5 = Convolution1D(nb_filter=nb_filter, filter_length=filter_kernels[5],
+                          border_mode='valid', activation='relu')(conv4)
+    conv5 = MaxPooling1D(pool_length=3)(conv5)
+    conv5 = Flatten()(conv5)
+
+    #Two dense layers with dropout of .5
+    z = Dropout(0.5)(Dense(dense_outputs, activation='relu')(conv5))
+    z = Dropout(0.5)(Dense(dense_outputs, activation='relu')(z))
+
+    #Output dense layer with softmax activation
+    pred = Dense(cat_output, activation='softmax', name='output')(z)
+
+    model = Model(input=inputs, output=pred)
+
+    sgd = SGD(lr=0.01, momentum=0.9)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd,
+                  metrics=['accuracy'])
+
+    return model
