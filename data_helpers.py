4fa61b6b92ea900d0ed98008fc1ca38351346494
Fuyang Liu <liufu.ty@gmail.com>
2018-03-18 18:08:55 +0100

    Update to Keras v2.1.5 and use tensorflow
    Update code to be able to run on Keras v2.1.5
    Changes include update Keras api and specifiy
    weight initilization for convolutional layers

--- data_helpers.py
+++ data_helpers.py
@@ -24,67 +24,35 @@ def load_ag_data():
     return (x_train, y_train), (x_test, y_test)
 
 
-def mini_batch_generator(x, y, vocab, vocab_size, vocab_check, maxlen,
-                         batch_size=128):
+def encode_data(x, maxlen, vocab):
+    # Iterate over the loaded data and create a matrix of size (len(x), maxlen)
+    # Each character is encoded into a one-hot array later at the lambda layer.
+    # Chars not in the vocab are encoded as -1, into an all zero vector.
 
-    for i in xrange(0, len(x), batch_size):
-        x_sample = x[i:i + batch_size]
-        y_sample = y[i:i + batch_size]
-
-        input_data = encode_data(x_sample, maxlen, vocab, vocab_size,
-                                 vocab_check)
-
-        yield (input_data, y_sample)
-
-
-def encode_data(x, maxlen, vocab, vocab_size, check):
-    #Iterate over the loaded data and create a matrix of size maxlen x vocabsize
-    #In this case that will be 1014x69. This is then placed in a 3D matrix of size
-    #data_samples x maxlen x vocab_size. Each character is encoded into a one-hot
-    #array. Chars not in the vocab are encoded into an all zero vector.
-
-    input_data = np.zeros((len(x), maxlen, vocab_size))
+    input_data = np.zeros((len(x), maxlen), dtype=np.int)
     for dix, sent in enumerate(x):
         counter = 0
-        sent_array = np.zeros((maxlen, vocab_size))
-        chars = list(sent.lower().replace(' ', ''))
-        for c in chars:
+        for c in sent:
             if counter >= maxlen:
                 pass
             else:
-                char_array = np.zeros(vocab_size, dtype=np.int)
-                if c in check:
-                    ix = vocab[c]
-                    char_array[ix] = 1
-                sent_array[counter, :] = char_array
+                ix = vocab.get(c, -1)  # get index from vocab dictionary, if not in vocab, return -1
+                input_data[dix, counter] = ix
                 counter += 1
-        input_data[dix, :, :] = sent_array
-
     return input_data
 
 
-def shuffle_matrix(x, y):
-    stacked = np.hstack((np.matrix(x).T, y))
-    np.random.shuffle(stacked)
-    xi = np.array(stacked[:, 0]).flatten()
-    yi = np.array(stacked[:, 1:])
-
-    return xi, yi
-
-
 def create_vocab_set():
-    #This alphabet is 69 chars vs. 70 reported in the paper since they include two
+    # This alphabet is 69 chars vs. 70 reported in the paper since they include two
     # '-' characters. See https://github.com/zhangxiangxiao/Crepe#issues.
 
-    alphabet = (list(string.ascii_lowercase) + list(string.digits) +
-                list(string.punctuation) + ['\n'])
+    alphabet = set(list(string.ascii_lowercase) + list(string.digits) +
+                   list(string.punctuation) + ['\n'])
     vocab_size = len(alphabet)
-    check = set(alphabet)
-
     vocab = {}
     reverse_vocab = {}
     for ix, t in enumerate(alphabet):
         vocab[t] = ix
         reverse_vocab[ix] = t
 
-    return vocab, reverse_vocab, vocab_size, check
+    return vocab, reverse_vocab, vocab_size, alphabet
--- py_crepe.py
+++ py_crepe.py
@@ -1,49 +1,73 @@
 from keras.models import Model
-from keras.optimizers import SGD
-from keras.layers import Input, Dense, Dropout, Flatten
+from keras.optimizers import SGD, Adam
+from keras.layers import Input, Dense, Dropout, Flatten, Lambda, Embedding
 from keras.layers.convolutional import Convolution1D, MaxPooling1D
+from keras.initializers import RandomNormal
 
 
-def model(filter_kernels, dense_outputs, maxlen, vocab_size, nb_filter,
-          cat_output):
-    #Define what the input shape looks like
-    inputs = Input(shape=(maxlen, vocab_size), name='input', dtype='float32')
+def create_model(filter_kernels, dense_outputs, maxlen, vocab_size, nb_filter, cat_output):
+    initializer = RandomNormal(mean=0.0, stddev=0.05, seed=None)
 
-    #All the convolutional layers...
-    conv = Convolution1D(nb_filter=nb_filter, filter_length=filter_kernels[0],
-                         border_mode='valid', activation='relu',
-                         input_shape=(maxlen, vocab_size))(inputs)
-    conv = MaxPooling1D(pool_length=3)(conv)
+    # Define what the input shape looks like
+    inputs = Input(shape=(maxlen,), dtype='int64')
 
-    conv1 = Convolution1D(nb_filter=nb_filter, filter_length=filter_kernels[1],
-                          border_mode='valid', activation='relu')(conv)
-    conv1 = MaxPooling1D(pool_length=3)(conv1)
+    # Option one:
+    # Uncomment following code to use a lambda layer to create a onehot encoding of a sequence of characters on the fly.
+    # Holding one-hot encodings in memory is very inefficient.
+    # The output_shape of embedded layer will be: batch x maxlen x vocab_size
+    #
+    import tensorflow as tf
 
-    conv2 = Convolution1D(nb_filter=nb_filter, filter_length=filter_kernels[2],
-                          border_mode='valid', activation='relu')(conv1)
+    def one_hot(x):
+        return tf.one_hot(x, vocab_size, on_value=1.0, off_value=0.0, axis=-1, dtype=tf.float32)
 
-    conv3 = Convolution1D(nb_filter=nb_filter, filter_length=filter_kernels[3],
-                          border_mode='valid', activation='relu')(conv2)
+    def one_hot_outshape(in_shape):
+        return in_shape[0], in_shape[1], vocab_size
 
-    conv4 = Convolution1D(nb_filter=nb_filter, filter_length=filter_kernels[4],
-                          border_mode='valid', activation='relu')(conv3)
+    embedded = Lambda(one_hot, output_shape=one_hot_outshape)(inputs)
 
-    conv5 = Convolution1D(nb_filter=nb_filter, filter_length=filter_kernels[5],
-                          border_mode='valid', activation='relu')(conv4)
-    conv5 = MaxPooling1D(pool_length=3)(conv5)
+    # Option two:
+    # Or, simply use Embedding layer as following instead of use lambda to create one-hot layer
+    # Think of it as a one-hot embedding and a linear layer mashed into a single layer.
+    # See discussion here: https://github.com/keras-team/keras/issues/4838
+    # Note this will introduce one extra layer of weights (of size vocab_size x vocab_size = 69*69 = 4761)
+    # embedded = Embedding(input_dim=vocab_size, output_dim=vocab_size)(inputs)
+
+    # All the convolutional layers...
+    conv = Convolution1D(filters=nb_filter, kernel_size=filter_kernels[0], kernel_initializer=initializer,
+                         padding='valid', activation='relu',
+                         input_shape=(maxlen, vocab_size))(embedded)
+    conv = MaxPooling1D(pool_size=3)(conv)
+
+    conv1 = Convolution1D(filters=nb_filter, kernel_size=filter_kernels[1], kernel_initializer=initializer,
+                          padding='valid', activation='relu')(conv)
+    conv1 = MaxPooling1D(pool_size=3)(conv1)
+
+    conv2 = Convolution1D(filters=nb_filter, kernel_size=filter_kernels[2], kernel_initializer=initializer,
+                          padding='valid', activation='relu')(conv1)
+
+    conv3 = Convolution1D(filters=nb_filter, kernel_size=filter_kernels[3], kernel_initializer=initializer,
+                          padding='valid', activation='relu')(conv2)
+
+    conv4 = Convolution1D(filters=nb_filter, kernel_size=filter_kernels[4], kernel_initializer=initializer,
+                          padding='valid', activation='relu')(conv3)
+
+    conv5 = Convolution1D(filters=nb_filter, kernel_size=filter_kernels[5], kernel_initializer=initializer,
+                          padding='valid', activation='relu')(conv4)
+    conv5 = MaxPooling1D(pool_size=3)(conv5)
     conv5 = Flatten()(conv5)
 
-    #Two dense layers with dropout of .5
+    # Two dense layers with dropout of .5
     z = Dropout(0.5)(Dense(dense_outputs, activation='relu')(conv5))
     z = Dropout(0.5)(Dense(dense_outputs, activation='relu')(z))
 
-    #Output dense layer with softmax activation
+    # Output dense layer with softmax activation
     pred = Dense(cat_output, activation='softmax', name='output')(z)
 
-    model = Model(input=inputs, output=pred)
+    model = Model(inputs=inputs, outputs=pred)
 
     sgd = SGD(lr=0.01, momentum=0.9)
-    model.compile(loss='categorical_crossentropy', optimizer=sgd,
-                  metrics=['accuracy'])
+    adam = Adam(lr=0.001)  # Feel free to use SGD above. I found Adam with lr=0.001 is faster than SGD with lr=0.01
+    model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
 
     return model
